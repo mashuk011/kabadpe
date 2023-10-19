dc7d0cabeedd768fd362aa493c9ea43895cda8b9
PaneruVipin <paneruvipinbusiness@gmail.com>
2023-10-18 22:20:03 -0700

    miner

--- src/Components/Auth/ProtectComp.jsx
+++ src/Components/Auth/ProtectComp.jsx
@@ -1,13 +1,8 @@
 import { useSelector } from "react-redux";
 
-const Protect = ({ children, protectType = "logout" }) => {
+const Protect = ({ children, reverse = false }) => {
   const { userInfo } = useSelector((s) => s.user);
-  const renderCondition =
-    protectType == "logout"
-      ? userInfo
-      : protectType == "login"
-      ? !userInfo
-      : null;
+  const renderCondition = !reverse ? userInfo : !userInfo;
   return <>{renderCondition ? children : null}</>;
 };
 
