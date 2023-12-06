fbec07515fafef9a8ac2b9f4abd49281f9a81fc6
PaneruVipin <paneruvipinbusiness@gmail.com>
2023-12-05 22:54:56 -0800

    miner

--- src/Components/WasteColectSearch.jsx
+++ src/Components/WasteColectSearch.jsx
@@ -1,48 +1,53 @@
-import React, { useState } from 'react'
-import '../style/Profile.css';
-import UserProfCards from './UserProfCards';
-import UserProfUpdates from './UserProfUpdates';
-import '../style/Support.css';
-import WasteColectCounter from './WasteColectCounter';
-import WasteAppoint from './WasteAppoint';
+import React, { useState } from "react";
+import "../style/Profile.css";
+import UserProfCards from "./UserProfCards";
+import UserProfUpdates from "./UserProfUpdates";
+import "../style/Support.css";
+import WasteColectCounter from "./WasteColectCounter";
+import WasteAppoint from "./WasteAppoint";
 
-
-const WasteColectSearch = ({onclickShowDetail}) => {
-    const [notBox , setNotBox] = useState(false);
+const WasteColectSearch = ({ onclickShowDetail }) => {
+  const [notBox, setNotBox] = useState(false);
   return (
     <>
-    <section className="use-prf-left-main-bx">
-    
-    <section className="top-user-prof-search-bx top-user-prof-search-bx2">
-    <div className="u-p-cont top-user-prof-search-bx-flex">
-<div className="left-user-prof-name-bx">
-    <h5>Hi, <span>Tom Holland</span></h5>
-</div>
+      <section className="use-prf-left-main-bx">
+        <section className="top-user-prof-search-bx top-user-prof-search-bx2">
+          <div className="u-p-cont top-user-prof-search-bx-flex">
+            <div className="left-user-prof-name-bx">
+              <h5>
+                Hi, <span>Tom Holland</span>
+              </h5>
+            </div>
 
-<div className="right-user-prof-search-flex-bx">
+            <div className="right-user-prof-search-flex-bx">
+              <div className="u-prf-srch-bx">
+                <input
+                  type="text"
+                  name="search"
+                  id="search"
+                  placeholder="Search..."
+                />
+              </div>
 
-    <div className="u-prf-srch-bx">
-        <input type="text" name='search' id='search' placeholder='Search...' />
-        </div>
+              <button onClick={onclickShowDetail} className="u-prf-btn-bx">
+                Edit Profile
+              </button>
 
-    <button onClick={onclickShowDetail} className="u-prf-btn-bx">
-        Edit Profile
-    </button>
+              <button className="u-prf-btn-bx u-prf-btn-bx5">Sale Waste</button>
 
-    <button  className="u-prf-btn-bx u-prf-btn-bx5">
-        Sale Waste
-    </button>
+              <button className="u-prf-btn-bx u-prf-btn-bx6">
+                Buy Waste (Guest)
+              </button>
 
-    <button  className="u-prf-btn-bx u-prf-btn-bx6">
-        Buy Waste (Guest)
-    </button>
-
-    <div  className="notif-main-box">
-                <div onClick={() => setNotBox(!notBox)} className="bell-icon bell-icon2">
+              <div className="notif-main-box">
+                <div
+                  onClick={() => setNotBox(!notBox)}
+                  className="bell-icon bell-icon2"
+                >
                   <i class="fa-regular fa-bell"></i>
                 </div>
 
-                <div className={ notBox ? "notif-box notactive" : "notif-box"}>
+                <div className={notBox ? "notif-box notactive" : "notif-box"}>
                   <div className="not-user-box">
                     <div className="left-not-box">
                       <img src="./images/customImg/team-2.jpg" alt="" />
@@ -89,7 +94,7 @@ const WasteColectSearch = ({onclickShowDetail}) => {
 
                   <div className="not-user-box">
                     <div className="left-not-box left-not-box2 left-not-box3">
-                    <i class="fa-solid fa-house"></i>
+                      <i class="fa-solid fa-house"></i>
                     </div>
 
                     <div className="right-not-box">
@@ -101,29 +106,18 @@ const WasteColectSearch = ({onclickShowDetail}) => {
                   <button className="sell-all-not-btn">
                     See all notifications
                   </button>
-                  
                 </div>
               </div>
+            </div>
+          </div>
+        </section>
 
-    
-    
-</div>
+        <WasteColectCounter />
 
-</div>
-</section>
-
-
-    
-  <WasteColectCounter />
-
-
-<WasteAppoint />
-    </section>
-
-   
+        <WasteAppoint />
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default WasteColectSearch
-
+export default WasteColectSearch;
--- src/apis/admins/user.js
+++ src/apis/admins/user.js
@@ -1,15 +1,19 @@
-export const userProfileImageAdd = resolvePromise(async (image) => {
-    const apiUrl = ENV_API_BASE_URL + `/user/profileimage`;
-    const token = getFromLocalStorage("token");
-    const { data: res } = await axios.post(
-      apiUrl,
-      { image },
-      {
-        headers: {
-          Authorization: token,
-          "Content-Type": "multipart/form-data",
-        },
-      }
-    );
-    return res?.message;
-  });
+import axios from "axios";
+import { resolvePromise } from "../../lib/http";
+import { getFromLocalStorage } from "../../lib/localStorage";
+
+export const collectorProfileImageAdd = resolvePromise(async (image) => {
+  const apiUrl = ENV_API_BASE_URL + `/collector/profileimage`;
+  const token = getFromLocalStorage("token");
+  const { data: res } = await axios.post(
+    apiUrl,
+    { image },
+    {
+      headers: {
+        Authorization: token,
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
+  return res?.message;
+});
